8aeb948dd76738b73aeecfeb6faceb8a851bae2d
Николай <wasilev-n@mail.ru>
2024-02-01 13:51:56 +0500

    section top swiper

--- frontend/src/componets/SwiperSection/index.tsx
+++ frontend/src/componets/SwiperSection/index.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "./style.css";
-import "swiper/css/scrollbar";
+// import "swiper/css/scrollbar";
 import styles from "./SwiperSection.module.scss";
 
 const arrSlider = [
@@ -58,11 +59,14 @@ export function SwiperSection() {
           disableOnInteraction: true,
         }}
         scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
+        onSwiper={(swiper) => {
+          swiper.updateSlidesClasses();
+          console.log(swiper);
+        }}
         onSlideChange={() => console.log("slide change")}>
         {arrSlider.map((item, index) => {
           return (
-            <SwiperSlide key={index}>
+            <SwiperSlide className={styles.swiper__block_item} key={index}>
               <div
                 className={styles["swiper__block_item"]}
                 style={{ backgroundImage: `url(${item.urlImg}` }}>
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -10,7 +10,25 @@ export function Home() {
     <main className={styles.main}>
       <div className="container">
         <section className={styles.section_top}>
+          <div
+            className={styles.section_top_item}
+            style={{ backgroundImage: "url(/img/job_top.jpg)" }}>
+            <h3>Собственное швейное производство</h3>
+            <p>Приглашаем к сотрудничеству</p>
+            <Link to="*">
+              Подробнее <HandySvg src="/img/right.svg" />
+            </Link>
+          </div>
           <SwiperSection />
+          <div
+            className={styles.section_top_item}
+            style={{ backgroundImage: "url(/img/showroom_top.jpg)" }}>
+            <h3>Посетите наш шоу-рум!</h3>
+            <p>Н.Новгород, ул.Народная 20 Ежедневно с 9:00 до 19:00</p>
+            <Link to="*">
+              Подробнее <HandySvg src="/img/right.svg" />
+            </Link>
+          </div>
         </section>
       </div>
     </main>
