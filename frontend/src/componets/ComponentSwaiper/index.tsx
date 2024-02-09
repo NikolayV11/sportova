84087dae940ccc28ee7f7512e9d0572f0d966f1d
Николай <wasilev-n@mail.ru>
2024-02-09 10:31:25 +0500

    оптимизация свапа завершина

--- frontend/src/componets/ComponentSwaiper/index.tsx
+++ frontend/src/componets/ComponentSwaiper/index.tsx
@@ -13,6 +13,11 @@ const img = [
   "/img/sw_3.jpeg",
   "/img/sw_2.jpeg",
   "/img/sw_3.jpeg",
+  "/img/sw_3.jpeg",
+  "/img/sw_2.jpeg",
+  "/img/sw_3.jpeg",
+  "/img/sw_2.jpeg",
+  "/img/sw_3.jpeg",
 ];
 export function ComponentSwaiper() {
   //swipe 1
@@ -20,8 +25,10 @@ export function ComponentSwaiper() {
   const [maxClick, setMaixClick] = React.useState(1);
   const [leftList, setLeftList] = React.useState(0);
   const legthItem = React.useRef<any | number>(0);
+
   function onclickPrev() {
     setClick(click - 1);
+    console.log(`leftList:${leftList}`);
     if (click <= 1) {
       setClick(maxClick);
       setLeftList(580 * (maxClick - 1));
@@ -31,9 +38,7 @@ export function ComponentSwaiper() {
   }
   function onclickNext() {
     setClick(click + 1);
-    console.log(`click ${click}, maxClick ${maxClick}`);
     if (click >= maxClick) {
-      console.log(`click ${click}, maxClick ${maxClick} if`);
       setLeftList(0);
       setClick(1);
       return;
@@ -42,36 +47,41 @@ export function ComponentSwaiper() {
   }
   // swipe 2
   const widthSwipeTwo = React.useRef<any>();
-  const [mouseSwipeTwo, setMouseSwipeTwo] = React.useState(null);
   const [swipeTwoStatus, setSwipeTwoStatus] = React.useState(false);
   const [coordinatesSwipeTwo, setCoordinatesSwipeTwo] = React.useState(null);
   const [leftListTwo, setLeftListTwo] = React.useState(0);
   const [widthSwipeList, setWidthSwipeList] = React.useState(0);
 
-  function swipeTwoListLeft(num: number) {
-    if (swipeTwoStatus) {
-      console.log(num);
-      setSwipeTwoStatus(false);
-    }
-  }
-  function swipeTwoListRight(num: number) {
-    if (swipeTwoStatus) {
-      console.log(num);
-      setSwipeTwoStatus(false);
-    }
-  }
   function swipeTwoStart(clientX: any) {
+    const maxWidth = widthSwipeList - (widthSwipeList - (maxClick - 4) * 150);
     if (coordinatesSwipeTwo > clientX) {
-      swipeTwoListLeft(1);
-      return;
+      if (swipeTwoStatus) {
+        setSwipeTwoStatus(false);
+        setCoordinatesSwipeTwo(null);
+        if (leftListTwo < maxWidth) {
+          setLeftListTwo(leftListTwo + 150);
+        }
+      }
     }
     if (coordinatesSwipeTwo < clientX) {
-      swipeTwoListRight(2);
-      return;
+      if (swipeTwoStatus) {
+        setSwipeTwoStatus(false);
+        setCoordinatesSwipeTwo(null);
+        if (leftListTwo > 0) {
+          setLeftListTwo(leftListTwo - 150);
+        }
+      }
     }
   }
 
+  // отлеживание нажатия на изображение из swipeTwo
+  function onClickSwipeTwo(index: number) {
+    setLeftList(580 * index);
+    setClick(index + 1);
+  }
+
   React.useEffect(() => {
+    // количество элементов в первом блоке
     setMaixClick(legthItem.current.childElementCount);
     setWidthSwipeList(widthSwipeTwo.current.offsetWidth);
   }, []);
@@ -89,6 +99,7 @@ export function ComponentSwaiper() {
               return (
                 <div key={index} className={styles.swiper_block_items__list_item}>
                   <img src={item} alt="" />
+                  <p>{`${click}/${maxClick}`}</p>
                 </div>
               );
             })}
@@ -103,14 +114,12 @@ export function ComponentSwaiper() {
             onDragStart={(event) => {
               setCoordinatesSwipeTwo(event.clientX);
               setSwipeTwoStatus(true);
-              // console.log("onDragStart", event.clientX);
             }}
             onDrag={(event) => {
               swipeTwoStart(event.clientX);
-              // console.log("onDrag провели удерживая", event.clientX);
             }}
-            onDragEnd={(event) => {
-              setCoordinatesSwipeTwo(null);
+            onDragEnd={() => {
+              return false;
             }}
             className={styles.swiper_two__list_items}
             style={{ left: `${-leftListTwo}px` }}>
@@ -118,12 +127,14 @@ export function ComponentSwaiper() {
               return (
                 <div
                   onClick={() => {
-                    console.log("click");
+                    onClickSwipeTwo(index);
                   }}
                   key={index}
-                  className={styles.swiper_two__list_items_item}>
+                  className={`${styles.swiper_two__list_items_item} ${
+                    index + 1 === click && styles.swiper_two__list_items_item_active
+                  }`}>
                   <img src={item} alt="" />
-                  <p>{index}</p>
+                  <p>{index + 1}</p>
                 </div>
               );
             })}
