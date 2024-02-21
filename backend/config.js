9484b045900eb80651a5aa4562bd1876bc5de066
Николай <wasilev-n@mail.ru>
2024-02-21 20:08:49 +0500

    подключение к базе данных MySQL через sync-mysql

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host: "127.0.0.1",
+  user: "root",
+  password: "",
+  database: "SPORTOVA",
+};
+// "swedish_walls";
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const syncMysql = require("sync-mysql");
+const CONFIG = require("./config");
 const app = express();
 const PORT = 3030;
 
-app.get("/", (req, res) => {
-  res.send("ok");
+function t02(path) {
+  const connection = new syncMysql(CONFIG);
+  let query = `SELECT * FROM ${path}`;
+  const result = connection.query(query);
+  return result;
+}
+app.get("/data/:path", (req, res) => {
+  console.log(req.params);
+  res.send(t02(req.params.path));
 });
 
 app.listen(PORT, () => {
