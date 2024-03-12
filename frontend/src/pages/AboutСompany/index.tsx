a9b4c2c8f3b55394d43572cb79629e0cb7e35b03
Николай <wasilev-n@mail.ru>
2024-03-12 21:41:13 +0500

    добавил блок о компании

--- frontend/src/pages/AboutСompany/index.tsx
+++ frontend/src/pages/AboutСompany/index.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 
 import styles from "./AboutСompany.module.scss";
-import { ComponentSwaiper, SlideCompany } from "../../componets";
+import { SlideCompany } from "../../componets";
 import { Link } from "react-router-dom";
+
+const infoBlock = [
+  "Узкая специализация. Мы знаем больше нюансов и потребностей клиента, чем магазины, которые продают все подряд. Соответственно, сможем лучше проконсультировать и подобрать необходимый Вам комплекс;",
+  "90% ассортимента держим в наличии на складе в Нижнем Новгороде и Москве;",
+  "Выставочный зал, где все оборудование надежно закреплено. Вы можете протестировать комплексы еще до покупки;",
+  "Если Вам необходима сборка шведской стенки, детской площадки или турника – мы предоставляем такую услугу. В компании работают опытные и аккуратные специалисты. Также мы оказываем услугу по укорачиванию шведской стенки;",
+  "Отправки в регионы РФ производим каждый день. Для большинства городов действует оплата при получении;",
+  "Если товар не подошел, Вы можете его обменять или вернуть в течение расширенного срока в 30 дней;",
+];
 
 export function AboutСompany() {
   return (
@@ -25,6 +34,24 @@ export function AboutСompany() {
             </p>
           </div>
           <SlideCompany />
+          <div className={styles.block_page__company}>
+            <div className={styles.block_page__company_info}>
+              <h2>
+                Коротко о <span>Sportova</span>
+              </h2>
+              <ul>
+                {infoBlock.map((item, index) => {
+                  return (
+                    <li key={index}>
+                      <p>{item}</p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div className={styles.block_page__company_privilege}></div>
+            <div className={styles.block_page__company_contact}></div>
+          </div>
         </div>
       </div>
     </div>
